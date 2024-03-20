21d450f3a541ee27593231a5d3b8841f71f69d07
alx-yng <alexyoung0119@gmail.com>
2024-03-20 16:53:21 -0500

    moved chevron buttons to wrapper to better represent structure of gallery

--- src/components/GalleryComponents/GalleryWrapper.tsx
+++ src/components/GalleryComponents/GalleryWrapper.tsx
@@ -1,5 +1,21 @@
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const GalleryWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className='flex items-center justify-center gap-8'>
+      <button className='text-8xl'>
+        <FontAwesomeIcon icon={faChevronLeft} color='white' />
+      </button>
+      {children}
+      <button className='text-8xl'>
+        <FontAwesomeIcon icon={faChevronRight} color='white' />
+      </button>
+    </div>
+  );
 };
 
 export default GalleryWrapper;
--- src/components/GalleryComponents/PhotoBox.tsx
+++ src/components/GalleryComponents/PhotoBox.tsx
@@ -1,19 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
-
 const PhotoBox = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='flex items-center justify-center gap-8'>
-      <button className='text-8xl'>
-        <FontAwesomeIcon icon={faChevronLeft} color='white' />
-      </button>
-      {children}
-      <button className='text-8xl'>
-        <FontAwesomeIcon icon={faChevronRight} color='white' />
-      </button>
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default PhotoBox;
