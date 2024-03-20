dbd32e4856cf30a8ef03f68b24060d953ebedc2b
alx-yng <alexyoung0119@gmail.com>
2024-03-20 16:25:03 -0500

    added footer and gallery component structure

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,9 +1,12 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Gallery = () => {
   return (
     <>
       <Header title='Gallery' />
+      <main className='w-full h-screen flex flex-col justify-center items-center'></main>
+      <Footer />
     </>
   );
 };
