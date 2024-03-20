84ce0e1ab1b38d9f80ad548f9487775be499ab21
alx-yng <alexyoung0119@gmail.com>
2024-03-20 16:41:21 -0500

    made header fixed to center main content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,12 +12,12 @@ export default function Header({
   return (
     <>
       <div
-        className={`grid place-items-center place-content-center grid-cols-3 py-4 ${textColor} ${bgColor}`}
+        className={`fixed w-full grid place-items-center place-content-center grid-cols-3 py-4 ${textColor} ${bgColor}`}
       >
-        <h1 className="text-2xl font-thin tracking-wider text-light">
-          <a href="/">Workshop</a>
+        <h1 className='text-2xl font-thin tracking-wider text-light'>
+          <a href='/'>Workshop</a>
         </h1>
-        <h2 className="text-5xl font-normal tracking-wider">{title}</h2>
+        <h2 className='text-5xl font-normal tracking-wider'>{title}</h2>
         <Nav links={links} textColor={textColor} />
       </div>
     </>
