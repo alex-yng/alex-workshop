7775dfe332e4ce0ce78bc188b0dbb3087b6b833f
alx-yng <alexyoung0119@gmail.com>
2024-03-21 12:23:21 -0500

    added images and fixed key error in navlinks

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,26 +7,22 @@ export default function Nav({
   ],
 }) {
   return (
-    <>
-      <nav className={`flex gap-4 text-xl font-${fontWeight} tracking-widest`}>
-        {links.map((link) => {
-          return (
-            <>
-              <div className="group">
-                <a
-                  className="hover:text-slate-400  group-hover:text-violet transition-colors duration-500"
-                  href={link.path}
-                >
-                  {link.name}
-                </a>
-                <hr
-                  className={`text-${textColor} w-0 group-hover:w-full transition-[width] duration-300`}
-                ></hr>
-              </div>
-            </>
-          );
-        })}
-      </nav>
-    </>
+    <nav className={`flex gap-4 text-xl font-${fontWeight} tracking-widest`}>
+      {links.map((link) => {
+        return (
+          <div className='group' key={link.name}>
+            <a
+              className='hover:text-slate-400  group-hover:text-violet transition-colors duration-500'
+              href={link.path}
+            >
+              {link.name}
+            </a>
+            <hr
+              className={`text-${textColor} w-0 group-hover:w-full transition-[width] duration-300`}
+            ></hr>
+          </div>
+        );
+      })}
+    </nav>
   );
 }
