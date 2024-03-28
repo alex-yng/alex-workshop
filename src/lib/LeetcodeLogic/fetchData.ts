31c5651aa063ca1600a0415c3754ffd0af5cbd11
alx-yng <alexyoung0119@gmail.com>
2024-03-27 19:28:59 -0500

    editing stats interface

--- src/lib/LeetcodeLogic/fetchData.ts
+++ src/lib/LeetcodeLogic/fetchData.ts
@@ -1,4 +1,22 @@
-export const fetchData = async (url: string) => {
+interface Stats {
+  status: string;
+  message: string;
+  totalSolved: number;
+  totalQuestions: number;
+  easySolved: number;
+  totalEasy: number;
+  mediumSolved: number;
+  totalMedium: number;
+  hardSolved: number;
+  totalHard: number;
+  acceptanceRate: number;
+  ranking: number;
+  contributionPoints: number;
+  reputation: number;
+  submissionCalendar: any;
+}
+
+export const fetchData = async (url: string): Promise<Stats> => {
   const response = await fetch(url);
   const data = await response.json();
   return data;
--- src/pages/Leetcode.tsx
+++ src/pages/Leetcode.tsx
@@ -1,17 +1,38 @@
+import { useEffect } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import fetchData from "../lib/LeetcodeLogic/fetchData";
+import fetchData, { Stats } from "../lib/LeetcodeLogic/fetchData";
 
 const Leetcode = () => {
-  const username = "alicks111";
-  const stats = fetchData(
-    `https://leetcode-stats-api.herokuapp.com/${username}`
-  );
+  let username = "alicks111";
+  let stats: Stats = {
+    status: "",
+    message: "",
+    totalSolved: 0,
+    totalQuestions: 0,
+    easySolved: 0,
+    totalEasy: 0,
+    mediumSolved: 0,
+    totalMedium: 0,
+    hardSolved: 0,
+    totalHard: 0,
+    acceptanceRate: 0,
+    ranking: 0,
+    contributionPoints: 0,
+    reputation: 0,
+    submissionCalendar: [],
+  };
+  useEffect(() => {
+    fetchData(`https://leetcode.com/${username}/`).then((data) => {
+      stats = data;
+    });
+  }, [username]);
+
   return (
     <>
       <Header />
       <main className='text-light h-screen grid place-items-center'>
-        Leetcode Tracker
+        <h1 className='text-white text-9xl'>{stats.totalSolved}</h1>
       </main>
       <Footer />
     </>
